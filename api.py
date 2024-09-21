5e9d3912daf54ce66ed7936c7f8b1bed7957ba53
Mayank Gupta <67818466+Mayank-Creater@users.noreply.github.com>
2024-09-21 18:09:38 +0000

    created a new api endpoint for exact data

--- api.py
+++ api.py
@@ -267,5 +267,189 @@ def companyData(inp):
     return ticker_name
     
 
+@app.route('/api2')
+def data2():
+    cookies = getcookie()
+
+    main_res = requests.get(url=url_main, headers=headers, cookies=cookies)
+    main_soup = BeautifulSoup(main_res.text, 'html.parser')
+
+    nf_price = main_soup.find_all('p', {'class': 'tb_val'})[0].text
+    nf_change = main_soup.find_all('p', {'class': 'tb_per'})[0].text
+    bnf_price = main_soup.find_all('p', {'class': 'tb_val'})[3].text
+    bnf_change = main_soup.find_all('p', {'class': 'tb_per'})[3].text
+
+    bnf_change = bnf_change.strip().replace('\r\n', '')
+    bnf_price = bnf_price.strip().replace('\n', '')
+    bnf_price = bnf_price.strip().replace(',', '')
+    nf_change = nf_change.strip().replace('\r\n', '')
+    nf_price = nf_price.strip().replace('\n', '')
+    nf_price = nf_price.strip().replace(',', '')
+
+    bnf_price = int(round(float(bnf_price)/1000, 1)*1000)
+    nf_price = int(round(float(nf_price)/1000, 1)*1000)
+
+    bnf_res = requests.get(url_bnf, headers=headers, cookies=cookies)
+    bnf_data = bnf_res.json()['filtered']
+
+    bnf_ce_totOI = bnf_data['CE']['totOI']
+    bnf_pe_totOI = bnf_data['PE']['totOI']
+    bnf_ce_coi = []
+    bnf_pe_coi = []
+    bnf_ce_vol = []
+    bnf_pe_vol = []
+    bnf_strike = []
+
+    for i in bnf_data['data']:
+        if (bnf_price-1000) <= i['strikePrice'] <= (bnf_price+1000):
+            bnf_strike.append(i['strikePrice'])
+            if 'CE' in i:
+                bnf_ce_coi.append(i['CE']['changeinOpenInterest'])
+                bnf_ce_vol.append(i['CE']['totalTradedVolume'])
+            if 'PE' in i:
+                bnf_pe_coi.append(i['PE']['changeinOpenInterest'])
+                bnf_pe_vol.append(i['PE']['totalTradedVolume'])
+
+    bnf_ce_coi = sum(bnf_ce_coi)
+    bnf_pe_coi = sum(bnf_pe_coi)
+
+    bnf_sorted_ce_vol = sorted(bnf_ce_vol)
+    bnf_sorted_pe_vol = sorted(bnf_pe_vol)
+
+    bnf_max_ce_vol = bnf_sorted_ce_vol[-1]
+    bnf_max_ce_vol2 = bnf_sorted_ce_vol[-2]
+    bnf_max_ce_vol3 = bnf_sorted_ce_vol[-3]
+
+    bnf_max_pe_vol = bnf_sorted_pe_vol[-1]
+    bnf_max_pe_vol2 = bnf_sorted_pe_vol[-2]
+    bnf_max_pe_vol3 = bnf_sorted_pe_vol[-3]
+
+    bnf_max_ce_vol_index = bnf_ce_vol.index(bnf_max_ce_vol)
+    bnf_max_ce_vol2_index = bnf_ce_vol.index(bnf_max_ce_vol2)
+    bnf_max_ce_vol3_index = bnf_ce_vol.index(bnf_max_ce_vol3)
+
+    bnf_max_ce_vol_strike_price = bnf_strike[bnf_max_ce_vol_index]
+    bnf_max_ce_vol2_strike_price = bnf_strike[bnf_max_ce_vol2_index]
+    bnf_max_ce_vol3_strike_price = bnf_strike[bnf_max_ce_vol3_index]
+
+    bnf_max_pe_vol_index = bnf_pe_vol.index(bnf_max_pe_vol)
+    bnf_max_pe_vol2_index = bnf_pe_vol.index(bnf_max_pe_vol2)
+    bnf_max_pe_vol3_index = bnf_pe_vol.index(bnf_max_pe_vol3)
+
+    bnf_max_pe_vol_strike_price = bnf_strike[bnf_max_pe_vol_index]
+    bnf_max_pe_vol2_strike_price = bnf_strike[bnf_max_pe_vol2_index]
+    bnf_max_pe_vol3_strike_price = bnf_strike[bnf_max_pe_vol3_index]
+
+
+
+    nf_res = requests.get(url_nf, headers=headers, cookies=cookies)
+    nf_data = nf_res.json()['filtered']
+
+    nf_ce_totOI = nf_data['CE']['totOI']
+    nf_pe_totOI = nf_data['PE']['totOI']
+    nf_ce_coi = []
+    nf_pe_coi = []
+    nf_ce_vol = []
+    nf_pe_vol = []
+    nf_strike = []
+
+    for j in nf_data['data']:
+        if (nf_price-1000) <= j['strikePrice'] <= (nf_price+1000):
+            nf_strike.append(j['strikePrice'])
+            if 'CE' in j:
+                nf_ce_coi.append(j['CE']['changeinOpenInterest'])
+                nf_ce_vol.append(j['CE']['totalTradedVolume'])
+            if 'PE' in j:
+                nf_pe_coi.append(j['PE']['changeinOpenInterest'])
+                nf_pe_vol.append(j['PE']['totalTradedVolume'])
+
+    nf_ce_coi = sum(nf_ce_coi)
+    nf_pe_coi = sum(nf_pe_coi)
+
+    nf_sorted_ce_vol = sorted(nf_ce_vol)
+    nf_sorted_pe_vol = sorted(nf_pe_vol)
+
+    nf_max_ce_vol = nf_sorted_ce_vol[-1]
+    nf_max_ce_vol2 = nf_sorted_ce_vol[-2]
+    nf_max_ce_vol3 = nf_sorted_ce_vol[-3]
+
+    nf_max_pe_vol = nf_sorted_pe_vol[-1]
+    nf_max_pe_vol2 = nf_sorted_pe_vol[-2]
+    nf_max_pe_vol3 = nf_sorted_pe_vol[-3]
+
+    nf_max_ce_vol_index = nf_ce_vol.index(nf_max_ce_vol)
+    nf_max_ce_vol2_index = nf_ce_vol.index(nf_max_ce_vol2)
+    nf_max_ce_vol3_index = nf_ce_vol.index(nf_max_ce_vol3)
+
+    nf_max_ce_vol_strike_price = nf_strike[nf_max_ce_vol_index]
+    nf_max_ce_vol2_strike_price = nf_strike[nf_max_ce_vol2_index]
+    nf_max_ce_vol3_strike_price = nf_strike[nf_max_ce_vol3_index]
+
+    nf_max_pe_vol_index = nf_pe_vol.index(nf_max_pe_vol)
+    nf_max_pe_vol2_index = nf_pe_vol.index(nf_max_pe_vol2)
+    nf_max_pe_vol3_index = nf_pe_vol.index(nf_max_pe_vol3)
+
+    nf_max_pe_vol_strike_price = nf_strike[nf_max_pe_vol_index]
+    nf_max_pe_vol2_strike_price = nf_strike[nf_max_pe_vol2_index]
+    nf_max_pe_vol3_strike_price = nf_strike[nf_max_pe_vol3_index]
+
+    gift_res = requests.get(url_gift)
+    soup = BeautifulSoup(gift_res.text, 'html.parser')
+    gift_change = soup.find_all('td', {'class': 'main-change'})[1].text
+    gift_change_value = gift_change.strip()
+
+    result = {
+        "BNF":{
+            "ce_totOI": bnf_ce_totOI,
+            "ce_coi": bnf_ce_coi,
+            "ce_max_vols": [
+                [bnf_max_ce_vol, bnf_max_ce_vol_strike_price],
+                [bnf_max_ce_vol2, bnf_max_ce_vol2_strike_price],
+                [bnf_max_ce_vol3, bnf_max_ce_vol3_strike_price]
+            ],
+            "ce_vol":bnf_ce_vol,
+            "pe_totOI": bnf_pe_totOI,
+            "pe_coi": bnf_pe_coi,
+            "pe_max_vols": [
+                [bnf_max_pe_vol, bnf_max_pe_vol_strike_price],
+                [bnf_max_pe_vol2, bnf_max_pe_vol2_strike_price],
+                [bnf_max_pe_vol3, bnf_max_pe_vol3_strike_price]
+            ],
+            "pe_vol":bnf_pe_vol,
+            "strike_price":bnf_strike,
+            "price": bnf_price,
+            "change": bnf_change
+        },
+        "NF":{
+            "ce_totOI": nf_ce_totOI,
+            "ce_coi": nf_ce_coi,
+            "ce_max_vols": [
+                [nf_max_ce_vol, nf_max_ce_vol_strike_price],
+                [nf_max_ce_vol2, nf_max_ce_vol2_strike_price],
+                [nf_max_ce_vol3, nf_max_ce_vol3_strike_price]
+            ],
+            "ce_vol": nf_ce_vol,
+            "pe_totOI": nf_pe_totOI,
+            "pe_coi": nf_pe_coi,
+            "pe_max_vols": [
+                [nf_max_pe_vol, nf_max_pe_vol_strike_price],
+                [nf_max_pe_vol2, nf_max_pe_vol2_strike_price],
+                [nf_max_pe_vol3, nf_max_pe_vol3_strike_price]
+            ],
+            "pe_vol": nf_pe_vol,
+            "strike_price": nf_strike,
+            "price": nf_price,
+            "change": nf_change
+        },
+        "GIFT NIFTY": gift_change_value,
+    }
+
+    response = jsonify(result)
+    response.headers.add('Access-Control-Allow-Origin','*')
+
+    return response
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
